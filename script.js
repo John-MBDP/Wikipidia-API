f3471bd1814724c0e5d9764226dd92be21d559ce
John <johnartuz@hotmail.com>
2022-05-13 00:26:01 -0400

    Create a params object by reading the API's documentation and understands what needs to be included

--- script.js
+++ script.js
@@ -14,6 +14,19 @@ const input = document.querySelector("#input");
 const errorSpan = document.querySelector("#error");
 const resultsContainer = document.querySelector("#results");
 
+const endpoint = "https://en.wikipedia.org/w/api.php?";
+const params = {
+  origin: "*",
+  format: "json",
+  action: "query",
+  prop: "extracts",
+  exchars: 250,
+  exintro: true,
+  explaintext: true,
+  generator: "search",
+  gsrlimit: 20,
+};
+
 const disableUi = () => {
   input.disabled = true;
   submitButton.disabled = true;
