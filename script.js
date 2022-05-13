10597b05abdcf925e055e1e8f99c66fc8ffd0a31
John <johnartuz@hotmail.com>
2022-05-13 01:07:07 -0400

    call the clearPreviousResult function to clear the results before making a new request

--- script.js
+++ script.js
@@ -50,15 +50,46 @@ const showError = (error) => {
   errorSpan.innerHTML = `❌${error}❌`;
 };
 
+const showResults = (results) => {
+  results.forEach((result) => {
+    resultsContainer.innerHTML += `
+      <div class="results__item">
+          <a href="https://en.wikipedia.org/?curid=${result.pageId}" target="_blank" class="card animated bounceInUp">
+              <h2 class="results__item__title">${result.title}</h2>
+              <p class="results__item__intro">${result.intro}</p>
+          </a>
+      </div>
+  `;
+  });
+};
+
+const gatherData = (pages) => {
+  const results = Object.values(pages).map((page) => ({
+    pageId: page.pageid,
+    title: page.title,
+    intro: page.extract,
+  }));
+  showResults(results);
+};
+
 const getData = async () => {
   const userInput = input.value;
   if (isInputEmpty(userInput)) return;
   params.gsrsearch = userInput;
+  clearPreviousResults();
   disableUi();
 
-  const { data } = await axios.get(endpoint, { params });
+  try {
+    const { data } = await axios.get(endpoint, { params });
 
-  console.log(data);
+    if (data.error) throw new Error(data.error.info);
+    gatherData(data.query.pages);
+  } catch (err) {
+    showError(err);
+    //finally will be called regardless of try or catch
+  } finally {
+    enableUi();
+  }
 };
 
 const handleKeyEvent = (e) => {
